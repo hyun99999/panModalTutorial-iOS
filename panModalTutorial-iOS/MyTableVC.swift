0cab35140c4ecd8b80a187c80275311bc4b7b37c
hyungyu Kim <ypsophila97@icloud.com>
2021-03-12 23:49:10 +0900

    Implement tableModal to show

--- panModalTutorial-iOS/MyTableVC.swift
+++ panModalTutorial-iOS/MyTableVC.swift
@@ -7,10 +7,19 @@
 
 import Foundation
 import UIKit
+import PanModal
+
 class MyTableVC: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+}
+
+extension MyTableVC: PanModalPresentable{
+    var panScrollable: UIScrollView? {
+        return tableView
+    }
+    
     
 }
 
--- panModalTutorial-iOS/ViewController.swift
+++ panModalTutorial-iOS/ViewController.swift
@@ -23,6 +23,10 @@ class ViewController: UIViewController {
         switch sender {
         case tableModalBtn:
             print("tableModalBtn 클릭")
+            
+            //스토리보드에서 뷰 모달을 만들었기 때문에 가져와야한다.
+            let vc = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "MyTableVC") as! MyTableVC
+            presentPanModal(vc)
         case halfModalBtn:
             print("halfModalBtn 클릭")
         default:
