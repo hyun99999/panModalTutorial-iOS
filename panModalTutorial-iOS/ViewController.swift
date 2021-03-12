9c8dab512d25f3257928ecc9c905ac081d165ba9
hyungyu Kim <ypsophila97@icloud.com>
2021-03-12 23:31:27 +0900

    Implement Main.storyboard for UI

--- panModalTutorial-iOS/ViewController.swift
+++ panModalTutorial-iOS/ViewController.swift
@@ -6,14 +6,20 @@
 //
 
 import UIKit
+import PanModal
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var tableModalBtn: UIButton!
+    @IBOutlet weak var halfModalBtn: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func onModalBtn(_ sender: UIButton) {
+        print("ViewController - onModalBtnClicked()")
+    }
+    
 }
 
